b37489c76ff948d0bff0571b4bf259b7446dd1ce
Suryakant <suryakant.sharma@fatbit.in>
2018-10-08 14:54:48 +0530

    Initial Commit

---
+++ OMDBClientApplicationTests/OMDBClientApplicationTests.swift
@@ -0,0 +1,34 @@
+//
+//  OMDBClientApplicationTests.swift
+//  OMDBClientApplicationTests
+//
+//  Created by Mac mini on 10/8/18.
+//  Copyright © 2018 Mac mini. All rights reserved.
+//
+
+import XCTest
+@testable import OMDBClientApplication
+
+class OMDBClientApplicationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
